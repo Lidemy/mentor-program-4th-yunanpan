3d1677b540cffc91d47be98192e5d9fb1a81347b
PAN YUN AN <skyyuvi@gmail.com>
2020-06-26 17:21:35 +0800

    week-two hw draft

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,16 @@
 function capitalize(str) {
-  
+    // 首字母為小寫 -> 首字母轉大寫
+    if (str[0] >= 'a' && str[0] <= 'z') {
+        let s = ''
+        let code = str.charCodeAt(0)
+        s += String.fromCharCode(code - 32)
+        for (let i=1; i<str.length; i++) {
+            s += str[i]
+        }
+        return s
+    } else {  // 其他情況維持原字串
+        return str
+    }
 }
 
 console.log(capitalize('hello'));
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,32 @@
 function join(arr, concatStr) {
-  
+    let str = ''
+    // 拿出陣列中的字
+    for (let i=0; i<arr.length; i++) {
+        // 最後不會有標點符號
+        if (i !== arr.length-1) {
+            str += arr[i]
+            str += concatStr
+        } else {
+            str += arr[i]
+        } 
+    }
+    return str
 }
 
 function repeat(str, times) {
-  
+    let repeatStr = ''
+    for (let i=1; i<=times; i++) {
+        repeatStr += str
+    }
+    return repeatStr
 }
 
 console.log(join(['a'], '!'));
+
+// console.log(join([1, 2, 3], '')) // 正確回傳值：123
+// console.log(join(["a", "b", "c"], "!")) // 正確回傳值：a!b!c
+// console.log(join(["a", 1, "b", 2, "c", 3], ',')) // 正確回傳值：a,1,b,2,c,3
+
+
 console.log(repeat('a', 5));
+// console.log(repeat('yoyo', 2)) // 正確回傳值：yoyoyoyo
