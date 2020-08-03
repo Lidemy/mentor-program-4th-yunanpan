3dba5c594a4234a5cb7bf2ccbfd19b1302a6c312
PAN YUN AN <skyyuvi@gmail.com>
2020-08-03 22:23:11 +0800

    week7 hw1 edit

--- homeworks/week7/hw1/js/all.js
+++ homeworks/week7/hw1/js/all.js
@@ -1,78 +1,45 @@
-const name = document.querySelector('#name');
-const email = document.querySelector('#email');
-const number = document.querySelector('#number');
-const how = document.querySelector('#how');
-const other = document.querySelector('#other');
-const radio = document.querySelectorAll('input[name=types]');
-// const imagination = document.querySelector('#imagination');
-// const readyMake = document.querySelector('#readyMake');
+/* eslint-disable no-alert */
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  let hasError = false; // 用於判斷是否 required 的表格都有填寫
+  const alertMessage = []; // 裝問題與回答
 
-function valification(option) {
-  let ans = [];
-  if (option.value.length < 1) {
-    // 沒通過
-    option.nextElementSibling.classList.remove('remind__off');
-    option.nextElementSibling.classList.add('remind__on');
-    return;
-  }
-  // 有通過
-  option.nextElementSibling.classList.remove('remind__on');
-  option.nextElementSibling.classList.add('remind__off');
-  const question = option.previousElementSibling.innerText;
-  ans = { question, answer: option.value };
-  return ans;// eslint-disable-line consistent-return
-}
+  // 先處理 input=text
+  const inputs = document.querySelectorAll('.required input[type=text]');
 
-document.querySelector('.form__submit').addEventListener('click',
-  () => {
-    const nameAns = valification(name);
-    const emailAns = valification(email);
-    const numberAns = valification(number);
-    const howAns = valification(how);
-
-    // radio 選項
-    let sum = 0;
-    let radioAns = null;
-    for (let i = 0; i < radio.length; i += 1) {
-      if (radio[i].checked) {
-        const question = document.querySelector('#type').innerText;
-        const answer = radio[i].closest('label').innerText;
-        radioAns = { question, answer };
-        sum += 1;
-      }
-    }
-
-    const formRadio = document.querySelectorAll('.form__radio');
-    if (sum < 1) {
-      formRadio[formRadio.length - 1].nextElementSibling.classList.remove('remind__off');
-      formRadio[formRadio.length - 1].nextElementSibling.classList.add('remind__on');
+  const answerText = function (input) {
+    if (input.value.trim().length === 0) { // 有輸入值且不能是空白
+      input.parentNode.classList.remove('hide');
+      hasError = true;
     } else {
-      formRadio[formRadio.length - 1].nextElementSibling.classList.remove('remind__on');
-      formRadio[formRadio.length - 1].nextElementSibling.classList.add('remind__off');
+      input.parentNode.classList.add('hide');
+      const question = input.parentNode.querySelector('label').innerText;
+      alertMessage.push({ question, answer: input.value });
     }
+  };
+  inputs.forEach(input => answerText(input));
 
-    // Other 選項
-    let otherAns = null;
-    if (other.value) {
-      const question = other.closest('label').children[0].innerText;
-      otherAns = { question, answer: other.value };
-    }
+  // radio 的情況
+  const radiosCheck = document.querySelector('.required input[type=radio]:checked');
+  const radios = document.querySelector('.required input[type=radio]');
+  if (radiosCheck) {
+    radios.parentNode.parentNode.classList.add('hide');
+    const question = radios.parentNode.parentNode.querySelector('div').innerText;
+    const answer = radiosCheck.parentNode.innerText;
+    alertMessage.push({ question, answer });
+    // alertMessage[question] = answer
+  } else {
+    radios.parentNode.parentNode.classList.remove('hide');
+    hasError = true;
+  }
 
-    function isValue(option) {
-      let str = '';
-      if (option) {
-        str += `${option.question}： ${option.answer} \n`;
-      }
-      return str;
-    }
-
-    // 放填寫資料
-    let alertMessege = '';
-    alertMessege
-    += isValue(nameAns) + isValue(emailAns)
-      + isValue(numberAns) + isValue(radioAns)
-      + isValue(howAns) + isValue(otherAns);
-    if (document.querySelectorAll('.remind__on').length === 0) { // 要確認都有資料才會 alert
-      alert(alertMessege);// eslint-disable-line no-alert
-    }
+  // 把問題和回答改成一行行的格式
+  let str = '';
+  alertMessage.forEach((message) => {
+    str += `${message.question}： ${message.answer} \n`;
   });
+
+  if (!hasError) {
+    alert(str);
+  }
+});
